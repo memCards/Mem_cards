c4eb732914aa2f89cc689dd175539fd52b8ef255
bratizgut <zonovka1910@gmail.com>
2021-04-25 10:37:45 +0700

    fix imports

--- src/main/java/view/NewCardForm.java
+++ src/main/java/view/NewCardForm.java
@@ -6,8 +6,6 @@ import ui.ButtonStyle;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class NewCardForm extends JFrame {
     private JLabel questLabel;
